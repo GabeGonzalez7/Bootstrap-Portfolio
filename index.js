9249336f178cbf37f9ba4d80d1ed646c0067802a
Gabe <gabrielgonzalez7@yahoo.com>
2019-01-14 20:57:45 -0800

    Scrollspy and Modal

---
+++ index.js
@@ -0,0 +1,32 @@
+// (function scrollReveal() {
+//     window.sr = ScrollReveal();
+    
+//     sr.reveal('.card', {
+//       duration   : 600,
+//       distance   : '20px',
+//       easing     : 'ease-out',
+//       origin     : 'bottom',
+//       reset      : true,
+//       scale      : 1,
+//       viewFactor : 0,
+//       afterReveal  : revealChildren,
+//     }, 150);
+    
+//       var revealChildren = sr.reveal('.card-body, .card-title, .card-text', {
+//       duration   : 500,
+//       scale      : 1,
+//       distance   : '20px',
+//       origin     : 'bottom',
+//       reset      : true,
+//       easing     : 'ease-out',
+//       viewFactor : 1,
+//     }, 75);
+//   })();
+
+// $("#pencil").click(function () {
+//     $("#theme-stylesheet").attr("href", "https://bootswatch.com/4/sketchy/bootstrap.css");
+// });
+
+// $("#moon").click(function () {
+//     $("#theme-stylesheet").attr("href", "https://bootswatch.com/4/superhero/bootstrap.css");
+// });
